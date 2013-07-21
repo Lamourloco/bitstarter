dcd8bfe24d27a26028e9348244aa2f469716c884
Alexis Shimanoff <alexis.shimanoff@gmail.com>
2013-07-21 06:00:24 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var k = fs.readFileSync('./index.html','utf8');
+  response.send( k );
 });
 
 var port = process.env.PORT || 5000;
